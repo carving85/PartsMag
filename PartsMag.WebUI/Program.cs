25c6c3ab924040471e8a77d508da47504e9ed3ab
carving85 <carving888@gmail.com>
2024-03-02 23:23:53 +0100

    Change userName to commit

--- PartsMag.WebUI/Program.cs
+++ PartsMag.WebUI/Program.cs
@@ -12,7 +12,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may wantdd to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
